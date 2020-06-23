2b7e0f4199704fae81d577e852df7e7c82fc5972
Miheran <66087679+Miheran@users.noreply.github.com>
2020-06-23 23:25:32 +1000

    Add files via upload

--- Doggy Game Care App/app/src/main/java/com/example/doggycaregameappjava/MainActivity.java
+++ Doggy Game Care App/app/src/main/java/com/example/doggycaregameappjava/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
@@ -33,20 +34,21 @@ public class MainActivity extends AppCompatActivity {
     //How long a run goes for as listed in Milliseconds
     private static final long RUN_TIME = 6000;
 
-    //Buttons for Home Fragment
-    Button btnRunHome;
-    Button btnVideoHome;
-    Button btnSleep;
-    //Buttons for Run Fragments
-    Button btnHomeRun;
-    Button btnVideoRun;
+    //Buttons declaration for sections of the app
+    Button btnHome;
+    Button btnRun;
+    Button btnVideo;
+    //Button for the Dog activities
     Button btnRunStart;
-    //Buttons for Video Fragments
-    Button btnHomeVideo;
-    Button btnRunVideo;
-    Button btnPlay;
+    Button btnSleep;
     //Chronometer for the Run time
     Chronometer chrRunTime;
+    //Animation Image to use for dog
+    AnimationDrawable dogAnm;
+    ImageView dogImg;
+    //Video View
+    VideoView startVideo;
+
 
     //
     @Override
@@ -55,12 +57,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //btnVideoRun = findViewById(R.id.videorun);
-        btnHomeVideo = findViewById(R.id.homevideo);
-        btnRunVideo = findViewById(R.id.runvideo);
+        btnHome = findViewById(R.id.homebtn);
+        btnRun = findViewById(R.id.runbtn);
+        btnVideo = findViewById(R.id.videobtn);
         btnRunStart = findViewById(R.id.runstart);
+        btnSleep = findViewById(R.id.sleep);
+        chrRunTime = findViewById(R.id.runtime);
+        dogImg = (ImageView) findViewById(R.id.dogImg);
+        startVideo = findViewById(R.id.openVideo);
 
-        Fragment frag;
+        //old function from previous version
+        //Fragment frag;
 
+        //Setting up the video so it can be played
         VideoView videoView = findViewById(R.id.openVideo);
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.openvideo2;
         Uri uri = Uri.parse(videoPath);
@@ -69,6 +78,9 @@ public class MainActivity extends AppCompatActivity {
         videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
 
+        //Sets the dog animation to the Idle Animation
+        dogImg.setBackgroundResource(R.drawable.idleanimation);
+        dogAnm = (AnimationDrawable) dogImg.getBackground();
 
         //if (savedInstanceState == null) {
             //getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLO,
@@ -76,48 +88,65 @@ public class MainActivity extends AppCompatActivity {
         //}
     }
 
+    //A method to set everything invisble so that only the ones needed at the time to be visiible
+    public void visbility()
+    {
+        btnHome.setVisibility(View.INVISIBLE);
+        btnRun.setVisibility(View.INVISIBLE);
+        btnVideo.setVisibility(View.INVISIBLE);
+        btnRunStart.setVisibility(View.INVISIBLE);
+        chrRunTime.setVisibility(View.INVISIBLE);
+        dogImg.setVisibility(View.INVISIBLE);
+        startVideo.setVisibility(View.INVISIBLE);
+    }
+
+    //Starts a Service that alerts the owner when the dog finishes running
     public void onStartService(View v)
     {
         Intent serviceRun = new Intent(this, RunningService.class);
         serviceRun.putExtra("RunTime", RUN_TIME);
 
+        dogImg.setBackgroundResource(R.drawable.runanimation);
+        dogAnm = (AnimationDrawable) dogImg.getBackground();
+
         startService(serviceRun);
     }
 
-    public void homeFragment(View view)
+    public void homeSection(View view)
     {
-        /*FragmentManager manager = getSupportFragmentManager();
-        androidx.fragment.app.FragmentTransaction ft = manager.beginTransaction();
-        Fragment homeFrag = manager.findFragmentById(R.id.fragmentLO);
-        if(homeFrag.isHidden())
-        {
-            ft.show(homeFrag);
-            ft.commit();
-        }*/
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLO, new HomeFragment()).addToBackStack(null).commit();
+        visbility();
+        //Set the required items as visible
+        btnRun.setVisibility(View.VISIBLE);
+        btnVideo.setVisibility(View.VISIBLE);
+        btnSleep.setVisibility(View.VISIBLE);
+        dogImg.setVisibility(View.VISIBLE);
     }
 
-    public void runFragment(View view)
+    public void runSection(View view)
     {
-        /*FragmentManager manager = getSupportFragmentManager();
-        androidx.fragment.app.FragmentTransaction ft = manager.beginTransaction();
-        Fragment homeFrag = manager.findFragmentById(R.id.fragmentLO);
-        if(homeFrag.isHidden())
-        {
-            ft.show(homeFrag);
-            ft.commit();
-        }*/
+        visbility();
+        //Set the required items as visible
+        btnHome.setVisibility(View.VISIBLE);
+        btnVideo.setVisibility(View.VISIBLE);
+        chrRunTime.setVisibility(View.VISIBLE);
         btnRunStart.setVisibility(View.VISIBLE);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLO, new RunFragment()).addToBackStack(null).commit();
+        dogImg.setVisibility(View.VISIBLE);
     }
 
-    public void videoFragment(View view)
+    public void videoSection(View view)
     {
-//        FragmentManager manager = getSupportFragmentManager();
-//        androidx.fragment.app.FragmentTransaction ft = manager.beginTransaction();
-//        Fragment homeFrag = manager.findFragmentById(R.id.fragmentLO);
-//        ft.hide(homeFrag);
-//        ft.commit();
+        visbility();
+        //Set the required items as visible
+        btnHome.setVisibility(View.VISIBLE);
+        btnRun.setVisibility(View.VISIBLE);
+        startVideo.setVisibility(View.VISIBLE);
+    }
+
+    public void sleeping(View v)
+    {
+        //Sets the Dog Animation to sleeping
+        dogImg.setBackgroundResource(R.drawable.sleepanimation);
+        dogAnm = (AnimationDrawable) dogImg.getBackground();
     }
 
 
